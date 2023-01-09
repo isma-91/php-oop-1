b3c13b75c1cac3008b40bea39df8fff3b663fcb1
isma-91 <isma91@hotmail.it>
2023-01-09 17:44:19 +0100

    Add both films and all properties

--- index.php
+++ index.php
@@ -19,10 +19,10 @@ class Movie {
   public $duration;
   public $production_company;
   public $plot;
-  // public $ticket_price = 11 + "€";
-  // public $imax = true;
+  public $ticket_price;
+  public $imax;
 
-  public function __construct($title, $director, $genre, $cast, $release_date, $duration, $production_company, $plot)
+  public function __construct($title, $director, $genre, $cast,$release_date, $duration, $production_company, $plot, $imax)
   {
     $this->title = $title;
     $this->director = $director;
@@ -32,24 +32,63 @@ class Movie {
     $this->duration = $duration;
     $this->production_company = $production_company;
     $this->plot = $plot;
-    // $this->ticket_price = setPrice($ticket_price, $imax);
-    // $this->imax = $imax;
+    $this->imax = $imax;
+    $this->setTicketPrice($imax);
   }
 
-  // public function setTicketPrice($imax)
-  // {
-  //   if (!($imax) && boolean) {
-  //     $this->ticket_price = "imax = " + 14 + "€";
-  //   }
-  // }
+  public function setTicketPrice($imax)
+  {
+    if ($imax === true) {
+      $this->ticket_price = 14.99;
+    } elseif ($imax === false) {
+      $this->ticket_price = 11.99;
+    }
+  }
 
-  // public function getTicketPrice()
-  // {
-  //   return $this->ticket_price;
-  // }  
+  public function getTicketPrice()
+  {
+    if ($this->imax === true) {
+      return "imax = " . $this->ticket_price . "€";
+    } elseif ($this->imax === false) {
+      return "normal = " . $this->ticket_price . "€";
+    }
+  }  
+
+  public function getCast(){
+    if ($this->cast && count($this->cast)) {
+      return implode(", ", $this->cast);
+    } else {
+      return "Nessun attore trovato";
+    }
+  
+  }
 }
 
-  $avatar = new Movie ('Avatar', 'James Cameron', 'sci-fi', ['Sam Worthington','Zoe Saldana', 'Stephen Lang', 'Michelle Rodriguez', 'Sigourney Weaver'], '2009', '162 mins', '20th Century Fox', 'On the lush alien world of Pandora live the Na\'vi, beings who appear primitive but are highly evolved. Because the planet\'s environment is poisonous, human/Na\'vi hybrids, called Avatars, must link to human minds to allow for free movement on Pandora. Jake Sully (Sam Worthington), a paralyzed former Marine, becomes mobile again through one such Avatar and falls in love with a Na\'vi woman (Zoe Saldana). As a bond with her grows, he is drawn into a battle for the survival of her world.');
+  $avatar = new Movie ('Avatar', 'James Cameron', 'sci-fi', ['Sam Worthington','Zoe Saldana', 'Stephen Lang', 'Michelle Rodriguez', 'Sigourney Weaver'], '2009', '162 mins', '20th Century Fox', 'Rinato nel corpo di un avatar, Jake può camminare di nuovo e dare inizio alla missione che gli è stata assegnata: infiltrarsi nel mondo dei Na\'vi, che sono diventati un serio ostacolo per le attività estrattive del prezioso minerale. Ma una bellissima donna Na\'vi, Neytiri, gli salva la vita e questo cambia tutto.', true);
 
-  echo $avatar->cast[1];
+  $titanic = new Movie ('Titanic', 'James Cameron', 'romantic', ['Leonardo DiCaprio', 'Kate Winslet', 'Billy Zane', 'Kathy Bates', 'Frances Fisher', 'Bernard Hill', 'Jonathan Hyde', 'Danny Nucci', 'David Warner', 'Bill Paxton'], '1997', '194 mins', 'Paramount Pictures', 'All\'epoca Rose (Kate Winslet) è una diciassettenne britannica che si imbarca con la madre Ruth (Frances Fisher) e lo spocchioso fidanzato Cal Hockley (Bill Zane), che è costretta a sposare per sanare i debiti della sua famiglia. Oppressa dal suo destino, Rose si arrampica sul parapetto, decisa a togliersi la vita.', false);
+
+  echo "<h1>Titolo: " . $avatar->title . "</h1>";
+  echo "<div> Regista: " . $avatar->director . "</div>";
+  echo "<div> Genere: " . $avatar->genre . "</div>";
+  echo "<div> Cast: " . $avatar->getCast() . "</div>";
+  echo "<div> Data di uscita: " . $avatar->release_date . "</div>";
+  echo "<div> Durata: " . $avatar->duration . "</div>";
+  echo "<div> Casa di produzione: " . $avatar->production_company . "</div>";
+  echo "<div> Trama: " . $avatar->plot . "</div>";
+  echo "<div> Prezzo: " . $avatar->getTicketPrice() . "</div>";
+
+  echo "<h1>Titolo: " . $titanic->title . "</h1>";
+  echo "<div> Regista: " . $titanic->director . "</div>";
+  echo "<div> Genere: " . $titanic->genre . "</div>";
+  echo "<div> Cast: " . $titanic->getCast() . "</div>";
+  echo "<div> Data di uscita: " . $titanic->release_date . "</div>";
+  echo "<div> Durata: " . $titanic->duration . "</div>";
+  echo "<div> Casa di produzione: " . $titanic->production_company . "</div>";
+  echo "<div> Trama: " . $titanic->plot . "</div>";
+  echo "<div> Prezzo: " . $titanic->getTicketPrice() . "</div>";
+  
   // var_dump($avatar);
+  // var_dump($titanic);
+
+  // ['Sam Worthington','Zoe Saldana', 'Stephen Lang', 'Michelle Rodriguez', 'Sigourney Weaver'],
